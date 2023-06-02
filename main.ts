4fe07a5910f94b9cab84611cb35d6eaf7b20e511
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2023-06-02 12:36:09 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+function llamada_manzana () {
+	
+}
+function llamada_inicio () {
+	
+}
+function llamada_final () {
+	
+}
+input.onButtonPressed(Button.AB, function () {
+    puntos = 0
+    espera = 500
+    cesta = 2
+    llamada_inicio()
+    juega = true
+    basic.pause(20000)
+    juega = false
+    llamada_final()
+})
+let cesta = 0
+let espera = 0
+let puntos = 0
+let juega = false
+juega = false
+basic.forever(function () {
+	
+})
