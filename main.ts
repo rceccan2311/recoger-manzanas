ea6b782da6a723a7310c21f7d5ccebb0ff7bad6b
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2023-06-15 08:52:46 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,17 @@ function llamada_manzana () {
 	
 }
 function llamada_inicio () {
-	
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    led.plot(cesta, 4)
 }
 function llamada_final () {
-	
+    basic.clearScreen()
+    basic.showNumber(puntos)
+    basic.pause(500)
+    basic.clearScreen()
 }
 input.onButtonPressed(Button.AB, function () {
     puntos = 0
@@ -17,11 +24,18 @@ input.onButtonPressed(Button.AB, function () {
     juega = false
     llamada_final()
 })
-let cesta = 0
 let espera = 0
 let puntos = 0
+let cesta = 0
 let juega = false
 juega = false
 basic.forever(function () {
-	
+    if (juega) {
+        let x = 0
+        llamada_manzana()
+        if (x == cesta) {
+            puntos = puntos + 1
+            espera = espera - 25
+        }
+    }
 })
